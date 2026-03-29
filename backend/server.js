f8398dca0fe190b32a591e01a18d64f20b8f27bc
Keerthana Shree Venugopal <keerthanashreevenugopal@gmail.com>
2026-03-29 23:16:51 +0530

    fix package.json

--- backend/server.js
+++ backend/server.js
@@ -229,8 +229,8 @@ ${article}
 } );
  
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
